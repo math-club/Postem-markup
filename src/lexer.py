8db24802996519aa09896cddc0ef7ee1f99f0177
le-grand-mannitout <ouaksel.madil@gmail.com>
2021-03-10 18:00:06 +0100

    Add the inline simple definition regex

--- src/lexer.py
+++ src/lexer.py
@@ -4,12 +4,13 @@ import re
 inline_marks = {
     r"_date": "Alias.abriged_date",
     r"_([\d]+)e": "Alias.century",
-    r"_([\d]+)": "Alias.line_numbering"
+    r"_([\d]+)": "Alias.line_numbering",
+    r"([^\n]+)[ \t]*::[ \t]*([^\n]+)": "Inline.simple_definition"
 }
 
 multiline_marks = {
     r"^\.\.[ \t]*([^\n]+)$": "Multiline.conclusion",
-    r"^>{1,6}[ \t]*([^\n]+)$": "Multiline.title"
+    r"^&{1,6}[ \t]*([^\n]+)$": "Multiline.title"
 }
 
 
