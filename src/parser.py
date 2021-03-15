853e9f528becd5512be10fc0eb99f9a94345b726
Tim-ats-d <tim.arnouts@protonmail.com>
2021-03-15 17:40:12 +0100

    Add more precise type hinting.

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,5 @@
 import re
-from typing import Any, Generator
+from typing import Any, Callable, Generator
 
 from marker import Alias, Inline, Multiline
 
@@ -8,25 +8,25 @@ Regex = str
 MarkedText = str
 
 
-inline_marks = {
+inline_marks: dict[Regex, Callable] = {
     r"_date": Alias.abriged_date,
     r"_([\d]+)e": Alias.century,
     r"_([\d]+)": Alias.line_numbering,
     r"([^\n]+)[ \t]*::[ \t]*([^\n]+)": Inline.simple_definition
 }
 
-multiline_marks = {
+multiline_marks: dict[Regex, Callable] = {
     r"^\.\.[ \t]*([^\n]+)$": Multiline.conclusion,
     r"^&{1,6}[ \t]*([^\n]+)$": Multiline.title
 }
 
 
-def compiles(iterable): #dict[Any, Regex]) -> Generator[Tuple[Any, Regex]]:
+def compiles(iterable: dict[Regex, Any]) -> Generator[Tuple[Regex, Any]]:
     return ((re.compile(regex), _)
                 for regex, _ in iterable.items())
 
 
-def parse_inline(text):
+def parse_inline(text: MarkedText) -> Generator:
     for nb, line in enumerate(text.split("\n")):
         for regex, mark in compiles(inline_marks):
             pattern = regex.search(line)
